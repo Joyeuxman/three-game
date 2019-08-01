18a3abb49424b5c31da67f29ada41752a98f3623
ligh <ligh@asiainfo.com>
2019-08-01 14:17:39 +0800

    使用three.js绘制旋转三角形

--- demo/demo-1/index.js
+++ demo/demo-1/index.js
@@ -1,3 +1,8 @@
+/*
+  原生webgl绘制一个旋转的三角形
+*/
+
+
 var canvas = document.getElementById('myCanvas');
 var gl = canvas.getContext('webgl');
 console.log('gl===', gl);
@@ -16,8 +21,6 @@ var FSHADER_SOURCE = `
     gl_FragColor = vec4(1.0,0.0,0.0,1.0);
   }
 `;
-var vertexShader;
-var fragmentShader;
 
 // create shader
 function createShader(gl, sourceCode, type) {
@@ -32,11 +35,11 @@ function createShader(gl, sourceCode, type) {
 
 // define vertex shader
 // 顶点着色器
-vertexShader = createShader(gl, VSHADER_SOURCE, gl.VERTEX_SHADER);
+var vertexShader = createShader(gl, VSHADER_SOURCE, gl.VERTEX_SHADER);
 
 // define fragmeng shader
 // 片元着色器
-fragmentShader = createShader(gl, FSHADER_SOURCE, gl.FRAGMENT_SHADER);
+var fragmentShader = createShader(gl, FSHADER_SOURCE, gl.FRAGMENT_SHADER);
 
 // attach program to shader
 // 把 WebGLShader 添加到 WebGLProgram
@@ -52,11 +55,13 @@ gl.program = program;
 
 // 将顶点位置信息灌入顶点着色器
 function initVertexBuffers(gl) {
+  // 顶点个数
+  var n = 3;
+  // 顶点位置信息
   var vertices = new Float32Array([0, 0.5, -0.5, -0.5, 0.5, -0.5]);
   // var vertices = new Float32Array([-1, 1, -1, -1, 1, -1]);
   // 创建 WebGLBuffer 对象
   var buffer = gl.createBuffer();
-  var n = 3;
   // 把 WebGLBuffer 对象绑定到指定目标上
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   // 更新缓冲数据
@@ -70,15 +75,18 @@ function initVertexBuffers(gl) {
 }
 
 var n = initVertexBuffers(gl);
+// 旋转的角度
 var currentAngle = 0;
 var g_last = Date.now();
 
-function tick() {
-  animate();
-  draw();
-  // 告诉浏览器——你希望执行一个动画，并且要求浏览器在下次重绘之前调用指定的回调函数更新动画。该方法需要传入一个回调函数作为参数，该回调函数会在浏览器下一次重绘之前执行
-  requestAnimationFrame(tick);
-}
+// 矩阵库
+var modelMatrix = new Matrix4();
+
+// 返回uniform 变量的指针位置
+var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+
+// 设置用于清空用的颜色
+gl.clearColor(0, 0, 0, 1);
 
 // 更新旋转的角度
 function animate() {
@@ -89,15 +97,6 @@ function animate() {
   currentAngle = currentAngle + (duration / 1000) * 180;
 }
 
-// 设置用于清空用的颜色
-gl.clearColor(0, 0, 0, 1);
-
-// 矩阵库
-var modelMatrix = new Matrix4();
-
-// 返回uniform 变量的指针位置
-var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-
 function draw() {
   // 根据坐标轴设置旋转角度
   modelMatrix.setRotate(currentAngle, 0, 1, 0);
@@ -107,6 +106,13 @@ function draw() {
   gl.clear(gl.COLOR_BUFFER_BIT);
   // 渲染数组中的原始数据
   gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+function tick() {
+  animate();
+  draw();
+  // 告诉浏览器——你希望执行一个动画，并且要求浏览器在下次重绘之前调用指定的回调函数更新动画。该方法需要传入一个回调函数作为参数，该回调函数会在浏览器下一次重绘之前执行
+  requestAnimationFrame(tick);
 }
 
 tick();
---
+++ demo/demo-2/index.js
@@ -0,0 +1,73 @@
+/*
+  three.js绘制旋转三角形
+*/
+
+var width = 400;
+var height = 400;
+
+var canvas = document.getElementById('myCanvas');
+// 渲染器
+// 渲染器决定了渲染的结果应该画在页面的什么元素上面，并且以怎样的方式来绘制
+var renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+});
+
+// 场景
+// 场景是所有物体的容器，如果要显示一个苹果，就需要将苹果对象加入场景中
+var sence = new THREE.Scene();
+
+// 正交相机
+// 相机决定了场景中那个角度的景色会显示出来。相机就像人的眼睛一样，人站在不同位置，抬头或者低头都能够看到不同的景色
+var camera = new THREE.OrthographicCamera(
+  -width / 2,
+  width / 2,
+  height / 2,
+  -height / 2,
+  -1000,
+  1000
+);
+
+renderer.setClearColor(new THREE.Color(0x000000, 1));
+renderer.setSize(400, 400);
+renderer.render(sence, camera);
+
+var triAngleShape = new THREE.Shape();
+triAngleShape.moveTo(0, 100);
+triAngleShape.lineTo(-100, -100);
+triAngleShape.lineTo(100, -100);
+triAngleShape.lineTo(0, 100);
+
+var geometry = new THREE.ShapeGeometry(triAngleShape);
+var material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide
+});
+var mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 0;
+mesh.position.y = 0;
+mesh.position.z = 1;
+sence.add(mesh);
+
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 0;
+camera.lookAt(new THREE.Vector3(0, 0, 1));
+
+var currentAngle = 0;
+var endTime = Date.now();
+
+function animate() {
+  var now = Date.now();
+  var duration = now - endTime;
+  endTime = now;
+  currentAngle = currentAngle + (duration / 1000) * Math.PI;
+}
+
+function render() {
+  animate();
+  mesh.rotation.set(0, 0, currentAngle);
+  renderer.render(sence, camera);
+  requestAnimationFrame(render);
+}
+
+render();
