00239aec219a1f0f7689e29abeec463eca19b871
ligh <ligh@asiainfo.com>
2019-08-01 19:14:38 +0800

    添加demo-2注释

--- demo/demo-2/index.js
+++ demo/demo-2/index.js
@@ -27,21 +27,33 @@ var camera = new THREE.OrthographicCamera(
   1000
 );
 
+// 设置清除的颜色
 renderer.setClearColor(new THREE.Color(0x000000, 1));
+// 将输出canvas的大小调整为(width, height)
 renderer.setSize(400, 400);
+// 用相机(camera)渲染一个场景(scene)
 renderer.render(sence, camera);
 
+// 从点来创建一个Shape  一个Vector2数组
 var triAngleShape = new THREE.Shape();
 triAngleShape.moveTo(0, 100);
 triAngleShape.lineTo(-100, -100);
 triAngleShape.lineTo(100, -100);
 triAngleShape.lineTo(0, 100);
 
+// 几何形
+// 从一个或多个路径形状中创建一个单面多边形几何体。
 var geometry = new THREE.ShapeGeometry(triAngleShape);
+// 基础网格材质
+// 一个以简单着色（平面或线框）方式来绘制几何体的材质
+// 这种材质不受光照的影响
 var material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
-  side: THREE.DoubleSide
+  side: THREE.DoubleSide // 双面
 });
+
+// 模型
+// 表示基于以三角形为polygon mesh（多边形网格）的物体的类
 var mesh = new THREE.Mesh(geometry, material);
 mesh.position.x = 0;
 mesh.position.y = 0;
@@ -65,6 +77,7 @@ function animate() {
 
 function render() {
   animate();
+  // 物体的局部旋转，以弧度来表示
   mesh.rotation.set(0, 0, currentAngle);
   renderer.render(sence, camera);
   requestAnimationFrame(render);
